69805befafa098aaaacfe149ed5327dac7b73a21
Jonathan Poelen <jonathan.poelen@gmail.com>
2021-12-10 16:13:13 +0100

    test.sh: fix coverage option

--- README.md
+++ README.md
@@ -14,6 +14,7 @@ npm i
 
 ```sh
 ./test.sh --coverage
+./test.sh --output-html
 ```
 
 # Uncompress kbdlayout.info.zst
--- test.sh
+++ test.sh
@@ -2,7 +2,8 @@
 set -e
 cd "$(dirname "$0")"
 case "$1" in
-  *help|-h) echo "$0 [--coverage|coverage]" ;;
-  *coverage) NODE_PATH=$PWD/lib ./node_modules/.bin/tap --coverage --coverage-map=map.js --coverage-report=html ;;
+  *help|-h) echo "$0 [--coverage|coverage | --output-html|output-html]" ;;
+  *coverage) NODE_PATH=$PWD/lib ./node_modules/.bin/tap --coverage --coverage-map=map.js ;;
+  *output-html) NODE_PATH=$PWD/lib ./node_modules/.bin/tap --coverage --coverage-map=map.js --coverage-report=html ;;
   *) NODE_PATH=$PWD/lib ./node_modules/.bin/tap --no-coverage ;;
 esac
